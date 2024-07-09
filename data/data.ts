c1fe98b4dcf6346fe19c69fa958e7e01bdd11be6
Wrseno <warsenosetyono@gmail.com>
2024-07-09 22:41:46 +0700

    feat:add static data

--- data/data.ts
+++ data/data.ts
@@ -73,3 +73,117 @@ export const events = [
       "Techcomfest adalah salah satu wujud misi dan peran aktif UKM Polytechnic Computer Club dalam mengikuti perkembangan teknologi dalam bidang IT. Dengan diadakannya kegiatan Techcomfest, diharapkan siswa/i SMA/SMK sederajat dan Mahasiswa/i tingkat nasional dapat menyalurkan inovasi dan kreativitas mereka khususnya dalam bidang teknologi dan informasi.",
   },
 ];
+
+// export const galleries = [
+//   {
+//     title: "cakra",
+//     image: "/images/IMG_8872.jpg",
+//     description:
+//       "Cakra adalah acara yang bertujuan untuk memperkenalkan UKM PCC kepada Para Mahasiswa Baru supaya Mahasiswa Baru mengetahui bahwa UKM PCC adalah UKM yang bergerak dibidang IT. Serta memperkenalkan apa yang terdapat didalam UKM PCC.",
+//   },
+//   {
+//     title: "open house",
+//     image: "/images/IMG_8872.jpg",
+//     description:
+//       "Open House bertujuan untuk memperkenalkan lebih dalam mengenai UKM PCC kepada calon anggota PCC. Memperkenalkan apa saja yang ada di PCC dan kegiatan-kegiatan lainnya yang sangat seru.",
+//   },
+//   {
+//     title: "hut pcc",
+//     image: "/images/IMG_8872.jpg",
+//     description:
+//       "Makrab bertujuan untuk menciptakan rasa kekeluargaan dan menjalin kerukunan antar pengurus UKM PCC, serta membuat para pengurus untuk saling mengenal satu sama lain.",
+//   },
+//   {
+//     title: "pendas & pab",
+//     image: "/images/IMG_8872.jpg",
+//     description:
+//       "Pendas dan PAB bertujuan untuk memberikan pendidikan dasar dan pembekalan kepada calon aktif UKM PCC.",
+//   },
+//   {
+//     title: "regenerasi",
+//     image: "/images/IMG_8872.jpg",
+//     description:
+//       "Regenerasi merupakan salah satu acara yang diadakan setahun sekali, acara ini sendiri adalah acara yang dilakukan bertujuan untuk peresmian dari pergantian anggota kepengurusan Struktur Organisasi dalam UKM PCC setelah diadakan Pengambilan Keputusan Bersama dari Seluruh Pengurus UKM PCC.",
+//   },
+//   {
+//     title: "makrab",
+//     image: "/images/IMG_8872.jpg",
+//     description:
+//       "Makrab bertujuan untuk menciptakan rasa kekeluargaan dan menjalin kerukunan antar pengurus UKM PCC, serta membuat para pengurus untuk saling mengenal satu sama lain.",
+//   },
+//   {
+//     title: "sertijab",
+//     image: "/images/IMG_8872.jpg",
+//     description:
+//       "Melantik pengurus PCC yang baru yang sebelumnya merupakan Anggota Aktif yang telah lolos melalui tahapan seleksi HRD dan Tim Formatur serta atas kebijakan dari Administrator dan melepas jabatan Pengurus UKM PCC yang telah selesai masa jabatannya sesuai dengan kesepakatan Anggaran Dasar.",
+//   },
+//   {
+//     title: "sidum & raker",
+//     image: "/images/IMG_8872.jpg",
+//     description:
+//       "Sidum dilaksanakan dengan tujuan membahas dan menetapkan AD/ART UKM PCC untuk satu tahun periode, sementara Raker dilaksanakan untuk menetapkan seluruh kegiatan selama satu tahun kepengurusan UKM PCC beserta seluruh anggota kepanitiaan dan tujuan yang ingin dicapai.",
+//   },
+//   {
+//     title: "techcomfest",
+//     image: "/images/IMG_8872.jpg",
+//     description:
+//       "Techcomfest adalah salah satu wujud misi dan peran aktif UKM Polytechnic Computer Club dalam mengikuti perkembangan teknologi dalam bidang IT. Dengan diadakannya kegiatan Techcomfest, diharapkan siswa/i SMA/SMK sederajat dan Mahasiswa/i tingkat nasional dapat menyalurkan inovasi dan kreativitas mereka khususnya dalam bidang teknologi dan informasi",
+//   },
+// ];
+
+export const galleries = [
+  {
+    title: "cakra",
+    image: "/images/IMG_8872.jpg",
+    description:
+      "Acara untuk memperkenalkan UKM PCC kepada Mahasiswa Baru dan bidang IT yang digeluti.",
+  },
+  {
+    title: "open house",
+    image: "/images/IMG_8872.jpg",
+    description:
+      "Memperkenalkan lebih dalam UKM PCC dan kegiatan-kegiatannya kepada calon anggota.",
+  },
+  {
+    title: "hut pcc",
+    image: "/images/IMG_8872.jpg",
+    description:
+      "Acara kekeluargaan untuk menjalin kerukunan dan saling mengenal antar pengurus UKM PCC.",
+  },
+  {
+    title: "pendas & pab",
+    image: "/images/IMG_8872.jpg",
+    description:
+      "Memberikan pendidikan dasar dan pembekalan kepada calon anggota aktif UKM PCC.",
+  },
+  {
+    title: "regenerasi",
+    image: "/images/IMG_8872.jpg",
+    description:
+      "Peresmian dan pergantian anggota kepengurusan UKM PCC setelah Pengambilan Keputusan Bersama.",
+  },
+  {
+    title: "makrab",
+    image: "/images/IMG_8872.jpg",
+    description:
+      "Acara kekeluargaan untuk menjalin kerukunan dan saling mengenal antar pengurus UKM PCC.",
+  },
+  {
+    title: "sertijab",
+    image: "/images/IMG_8872.jpg",
+    description:
+      "Pelantikan pengurus baru yang sebelumnya Anggota Aktif dan pelepasan jabatan pengurus lama sesuai AD/ART.",
+  },
+  {
+    title: "sidum & raker",
+    image: "/images/IMG_8872.jpg",
+    description:
+      "Sidum untuk menetapkan AD/ART, Raker untuk menetapkan kegiatan dan tujuan setahun UKM PCC.",
+  },
+  {
+    title: "techcomfest",
+    image: "/images/IMG_8872.jpg",
+    description:
+      "Acara tahunan UKM PCC untuk menyalurkan inovasi dan kreativitas dalam bidang teknologi dan informasi.",
+  },
+];
