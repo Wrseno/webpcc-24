3fb89d7f90e4f5f38db05a2bb78f71daa37218f5
Wrseno <warsenosetyono@gmail.com>
2024-07-09 22:46:17 +0700

    feat:add mission section components

--- components/atom/about/MissionSection.tsx
+++ components/atom/about/MissionSection.tsx
@@ -4,14 +4,15 @@ import {missions} from "@/data/data";
 export default function MissionSection() {
   return (
     <div className='px-4 md:px-16 lg:px-32 py-6 text-senary'>
-      <div className='bg-secondary shadow-lg shadow-septenary rounded-2xl'>
+      <div className='bg-secondary shadow-three rounded-2xl'>
         <div className='p-6 md:p-12'>
           <SectionTitle
             spanTitle='MISI'
             firstTitle='POLYTECHNIC'
-            useBr
             secondTitle='COMPUTER CLUB'
             spanColor='text-quinary'
+            useBr
+            useHr
           />
           <ol className='text-xs md:text-base'>
             {missions.map((miss, index) => (
