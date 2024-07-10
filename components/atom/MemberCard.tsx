789266f8c0c58837dffabcf4543f306d2f776e2b
Wrseno <warsenosetyono@gmail.com>
2024-07-10 07:24:12 +0700

    feat:add member card component

---
+++ components/atom/MemberCard.tsx
@@ -0,0 +1,22 @@
+import Image from "next/image";
+
+interface memberCardProps {
+  imageUrl: string;
+  alt: string;
+}
+
+export default function MemberCard({imageUrl, alt}: memberCardProps) {
+  return (
+    <div className='w-80 h-96 p-4 bg-secondary rounded-xl shadow-md overflow-hidden mx-auto'>
+      <div className='w-full h-full'>
+        <Image
+          src={imageUrl}
+          alt={alt}
+          width={1000}
+          height={1000}
+          className='rounded-xl'
+        />
+      </div>
+    </div>
+  );
+}
