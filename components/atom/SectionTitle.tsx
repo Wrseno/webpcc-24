f1218563fc2a78153ab577409f39f6a9be8b3ea7
Wrseno <warsenosetyono@gmail.com>
2024-07-10 07:22:55 +0700

    feat:add section title component

--- components/atom/SectionTitle.tsx
+++ components/atom/SectionTitle.tsx
@@ -5,6 +5,9 @@ interface SectionTitleProps {
   useBr: boolean;
   textColor?: string;
   spanColor?: string;
+  useLeftHr?: boolean;
+  useRightHr?: boolean;
+  useHr?: boolean;
 }
 
 export default function SectionTitle({
@@ -14,16 +17,29 @@ export default function SectionTitle({
   useBr,
   textColor,
   spanColor,
+  useLeftHr,
+  useRightHr,
+  useHr,
 }: SectionTitleProps) {
   return (
     <>
-      <h2 className={`${textColor} text-xl md:text-4xl font-bold`}>
-        <span className={`${spanColor} font-bold`}>{spanTitle} </span>{" "}
-        {firstTitle}
-        {useBr ? <br /> : ""}
-        {secondTitle}
-      </h2>
-      <hr className='w-1/2 border-quinary border-t-2 mb-4 md:border-t-4' />
+      <div className={`${useRightHr || useLeftHr ? "flex items-center" : ""}`}>
+        {useLeftHr && (
+          <hr className='w-1/2 border-primary border-t-2 mb-4 md:border-t-4' />
+        )}
+        <h2 className={`${textColor} text-xl md:text-4xl font-bold`}>
+          <span className={`${spanColor} font-bold`}>{spanTitle} </span>{" "}
+          {firstTitle}
+          {useBr ? <br /> : ""}
+          {secondTitle}
+        </h2>
+        {useRightHr && (
+          <hr className='w-1/2 border-primary border-t-2 mb-4 md:border-t-4' />
+        )}
+      </div>
+      {useHr && (
+        <hr className='w-1/2 border-quinary border-t-2 mb-4 md:border-t-4' />
+      )}
     </>
   );
 }
