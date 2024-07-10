c3042c83a5e1a422e359d16abe6fb81ef634bd8d
Wrseno <warsenosetyono@gmail.com>
2024-07-10 07:20:32 +0700

    feat:add all components

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,30 @@
-import {About, Event, Hero} from "@/components";
+import {
+  About,
+  Blog,
+  Event,
+  Element,
+  Gallery,
+  Hero,
+  Structure,
+} from "@/components";
 
 export default function Home() {
   return (
-    <main className=''>
+    <main className='container max-w-screen-2xl mx-auto '>
       <Hero />
       <About />
       <Event />
+      <div className='relative -my-40 md:my-56'>
+        <Element
+          src='icon-target.png'
+          alt='Icon target'
+          imgClassName='-left-1/2 md:w-[50%] md:-left-1/4 absolute'
+          isLeft
+        />
+      </div>
+      <Gallery />
+      <Structure />
+      <Blog />
     </main>
   );
 }
