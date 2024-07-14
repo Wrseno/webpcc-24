a000f07762fb57bcb121478e1d826a1af350c08c
Wrseno <warsenosetyono@gmail.com>
2024-07-14 21:43:34 +0700

    fix: fix components

--- app/page.tsx
+++ app/page.tsx
@@ -1,31 +1,21 @@
-"use client";
-
-import {useEffect} from "react";
-
-import {About, Blog, Event, Gallery, Hero, Structure} from "@/components";
-
-import AOS from "aos";
-import "aos/dist/aos.css";
+import {
+  AboutSection,
+  BlogSection,
+  EventSection,
+  GallerySection,
+  HeroSection,
+  StructureSection,
+} from "@/components";
 
 export default function Home() {
-  useEffect(() => {
-    AOS.init({
-      disable: function () {
-        var maxWidth = 800;
-        return window.innerWidth < maxWidth;
-      },
-    });
-    AOS.refresh();
-  }, []);
-
   return (
     <main className='container max-w-screen-2xl mx-auto flex flex-col gap-y-32'>
-      <Hero />
-      <About />
-      <Event />
-      <Gallery />
-      <Structure />
-      <Blog />
+      <HeroSection />
+      <AboutSection />
+      <EventSection />
+      <GallerySection />
+      <StructureSection />
+      <BlogSection />
     </main>
   );
 }
