b48037c892de66b85b51b2558e71e74dc19b1313
Wrseno <warsenosetyono@gmail.com>
2024-07-14 21:41:49 +0700

    feat: add kepengurusan page

--- app/kepengurusan/page.tsx
+++ app/kepengurusan/page.tsx
@@ -1,5 +1,41 @@
-import React from "react";
+import {
+  AdministratorSection,
+  BendaharaSection,
+  DepDanusSection,
+  DepMaintenanceSection,
+  DepMulmedSection,
+  DepSoftwareSection,
+  DepNetworkSection,
+  DivHrdSection,
+  DivHumasSection,
+  DivKrtSection,
+  DivRedaksiSection,
+  DivWorkshopSection,
+  LitbangSection,
+  SekretarisSection,
+  SekumSection,
+  StructureHero,
+} from "@/components";
 
 export default function page() {
-  return <div>page</div>;
+  return (
+    <main className='container max-w-screen-2xl mx-auto flex flex-col gap-y-32 py-32 px-4 md:px-24'>
+      <StructureHero />
+      <AdministratorSection />
+      <SekumSection />
+      <LitbangSection />
+      <SekretarisSection />
+      <BendaharaSection />
+      <DivHumasSection />
+      <DivHrdSection />
+      <DivRedaksiSection />
+      <DivKrtSection />
+      <DepDanusSection />
+      <DepMaintenanceSection />
+      <DivWorkshopSection />
+      <DepMulmedSection />
+      <DepSoftwareSection />
+      <DepNetworkSection />
+    </main>
+  );
 }
