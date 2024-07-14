845fbd7811a93cb70aa224ae1d2aae684e69e41e
Wrseno <warsenosetyono@gmail.com>
2024-07-14 21:44:30 +0700

    feat: add aoswrapper

---
+++ components/common/AosWrapper.tsx
@@ -0,0 +1,22 @@
+"use client";
+
+import {useEffect, ReactNode} from "react";
+import AOS from "aos";
+import "aos/dist/aos.css";
+
+interface AOSWrapperProps {
+  children: ReactNode;
+}
+
+export default function AOSWrapper({children}: AOSWrapperProps) {
+  useEffect(() => {
+    AOS.init({
+      duration: 1000,
+      once: true,
+      offset: 10,
+    });
+    AOS.refresh();
+  }, []);
+
+  return <>{children}</>;
+}
