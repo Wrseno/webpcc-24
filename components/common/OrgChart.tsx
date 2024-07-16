9552aaa15c78eecef8f2ea50dfc4704ff9363c8c
Wrseno <warsenosetyono@gmail.com>
2024-07-16 14:12:32 +0700

    feat: update organization use state

--- components/common/OrgChart.tsx
+++ components/common/OrgChart.tsx
@@ -6,129 +6,10 @@ import Link from "next/link";
 import {OrganizationChart} from "primereact/organizationchart";
 import {TreeNode} from "primereact/treenode";
 
+import {organization} from "@/data";
+
 export default function OrgChart() {
-  const [data] = useState([
-    {
-      expanded: true,
-      type: "person",
-      className: "bg-quinary text-senary p-4",
-      style: {borderRadius: "12px"},
-      data: {
-        title: "Administrator",
-        href: "#administrator",
-      },
-      children: [
-        {
-          expanded: true,
-          type: "person",
-          className: "bg-quaternary text-senary p-4",
-          style: {borderRadius: "12px"},
-          data: {
-            title: "Sekretaris Umum",
-            href: "#sekum",
-          },
-          children: [
-            {
-              label: "Sekretaris",
-              className: "bg-denary text-senary p-4",
-              style: {borderRadius: "12px"},
-              href: "#sekretaris",
-            },
-            {
-              expanded: true,
-              type: "person",
-              className: "bg-teartiary text-senary p-4",
-              style: {borderRadius: "12px"},
-              data: {
-                title: "Litbang",
-                href: "#litbang",
-              },
-              children: [
-                {
-                  label: "Divisi Humas",
-                  className: "bg-secondary text-senary p-4",
-                  style: {borderRadius: "12px"},
-                  href: "#humas",
-                },
-                {
-                  label: "Divisi HRD",
-                  className: "bg-secondary text-senary p-4",
-                  style: {borderRadius: "12px"},
-                  href: "#hrd",
-                },
-                {
-                  expanded: true,
-                  type: "person",
-                  className: "bg-secondary text-senary p-4",
-                  style: {borderRadius: "12px"},
-                  data: {
-                    title: "Divisi KRT",
-                    href: "#krt",
-                  },
-                  children: [
-                    {
-                      label: "Departemen Danus",
-                      className: "bg-primary text-senary p-4",
-                      style: {borderRadius: "12px"},
-                      href: "#danus",
-                    },
-                    {
-                      label: "Departemen Maintenance",
-                      className: "bg-primary text-senary p-4",
-                      style: {borderRadius: "12px"},
-                      href: "#maintenance",
-                    },
-                  ],
-                },
-                {
-                  label: "Divisi Redaksi",
-                  className: "bg-secondary text-senary p-4",
-                  style: {borderRadius: "12px"},
-                  href: "#redaksi",
-                },
-                {
-                  expanded: true,
-                  type: "person",
-                  className: "bg-secondary text-senary p-4",
-                  style: {borderRadius: "12px"},
-                  data: {
-                    title: "Divisi Workshop",
-                    href: "#workshop",
-                  },
-                  children: [
-                    {
-                      label: "Derpartemen Software",
-                      className: "bg-primary text-senary p-4",
-                      style: {borderRadius: "12px"},
-                      href: "#software",
-                    },
-                    {
-                      label: "Derpartemen Multimedia",
-                      className: "bg-primary text-senary p-4",
-                      style: {borderRadius: "12px"},
-                      href: "#multimedia",
-                    },
-                    {
-                      label: "Derpartemen Network",
-                      className: "bg-primary text-senary p-4",
-                      style: {borderRadius: "12px"},
-                      href: "#network",
-                    },
-                  ],
-                },
-              ],
-            },
-            {
-              label: "Bendahara",
-              className: "bg-denary text-senary p-4",
-              style: {borderRadius: "12px"},
-              href: "#bendahara",
-            },
-          ],
-        },
-      ],
-    },
-  ]);
+  const [data] = useState(organization);
 
   const nodeTemplate = (node: any) => {
     if (node.type === "person") {
@@ -145,7 +26,7 @@ export default function OrgChart() {
   };
 
   return (
-    <div className='card overflow-x-auto'>
+    <div className='card overflow-x-auto' data-aos='zoom-in'>
       <OrganizationChart value={data} nodeTemplate={nodeTemplate} />
     </div>
   );
