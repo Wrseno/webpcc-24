393f8ef97f6e12a6d7b910a924c535e726a4d31f
Wrseno <warsenosetyono@gmail.com>
2024-07-24 10:09:55 +0700

    feat: change section title children

--- components/pages/structure/sections/LitbangSection.tsx
+++ components/pages/structure/sections/LitbangSection.tsx
@@ -4,15 +4,17 @@ import {litbang} from "@/data/data";
 export default function LitbangSection() {
   return (
     <section className='h-auto py-16 px-4' id='litbang'>
-      <SectionTitle
-        firstTitle='DAN PENGEMBANGAN'
-        spanTitle='PENELITIAN'
-        spanColor='text-quinary'
-        textColor='text-primary'
-        useBr={false}
-        useLeftHr
-        hrWidth='w-full'
-      />
+      <SectionTitle useLeftHr hrWidth='w-full'>
+        <h2
+          className='text-2xl md:text-4xl font-bold mb-3 text-primary'
+          data-aos='fade-left'
+        >
+          <span className='font-bold text-quinary'>PENELITIAN </span>
+          DAN
+          <br />
+          PENGEMBANGAN
+        </h2>
+      </SectionTitle>
       <SevenMemberCard members={litbang} />
     </section>
   );
