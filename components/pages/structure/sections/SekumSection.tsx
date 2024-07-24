3debd972f8cc5135522023826a16366d1a205e6d
Wrseno <warsenosetyono@gmail.com>
2024-07-24 10:09:44 +0700

    feat: change section title children

--- components/pages/structure/sections/SekumSection.tsx
+++ components/pages/structure/sections/SekumSection.tsx
@@ -4,15 +4,16 @@ import SingleMemberCard from "../components/SingleMemberCard";
 export default function SekumSection() {
   return (
     <section className='h-auto py-16 px-4 md:px-24' id='sekum'>
-      <SectionTitle
-        firstTitle='UMUM'
-        spanTitle='SEKRETARIS'
-        textColor='text-primary'
-        spanColor='text-quinary'
-        useBr={false}
-        useRightHr
-        hrWidth='w-full'
-      />
+      <SectionTitle useRightHr hrWidth='w-full'>
+        <h2
+          className='text-2xl md:text-4xl font-bold mb-3 text-primary'
+          data-aos='fade-right'
+        >
+          <span className='font-bold text-quinary'>SEKRETARIS</span>
+          <br />
+          UMUM
+        </h2>
+      </SectionTitle>
       <SingleMemberCard
         memberImage='/images/sto/sekum.webp'
         memberName='Nor Aina'
