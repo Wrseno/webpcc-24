d5e18d486b54512f4c324570377a7c7142d4ad8d
Wrseno <warsenosetyono@gmail.com>
2024-07-25 15:50:02 +0700

    feat: change section title props

--- components/pages/structure/sections/BendaharaSection.tsx
+++ components/pages/structure/sections/BendaharaSection.tsx
@@ -5,15 +5,14 @@ import React from "react";
 export default function BendaharaSection() {
   return (
     <section className='h-auto py-16 px-4' id='bendahara'>
-      <SectionTitle
-        firstTitle='bendahara'
-        spanTitle=''
-        textColor='text-quinary'
-        useBr={false}
-        useLeftHr
-        useRightHr
-        hrWidth='w-full'
-      />
+      <SectionTitle useLeftHr useRightHr hrWidth='w-full'>
+        <h2
+          className='text-2xl md:text-4xl font-bold mb-3 text-quinary'
+          data-aos='fade-up'
+        >
+          BENDAHARA
+        </h2>
+      </SectionTitle>
 
       <ThreeMemberCard members={bendahara} />
     </section>
