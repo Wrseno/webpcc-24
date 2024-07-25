6748fb50f758e9d6ac9f5722a45fca4a54b55182
Wrseno <warsenosetyono@gmail.com>
2024-07-25 15:50:25 +0700

    feat: change section title props

--- components/pages/structure/sections/DepMaintenanceSection.tsx
+++ components/pages/structure/sections/DepMaintenanceSection.tsx
@@ -4,15 +4,16 @@ import {departemenMaintenance} from "@/data/data";
 export default function DepMaintenanceSection() {
   return (
     <section className='h-auto py-16 px-4 ' id='maintenance'>
-      <SectionTitle
-        firstTitle='maintenance'
-        spanTitle='departemen'
-        textColor='text-primary'
-        spanColor='text-quinary'
-        useBr={false}
-        useRightHr
-        hrWidth='w-full'
-      />
+      <SectionTitle useRightHr hrWidth='w-full'>
+        <h2
+          className='text-2xl md:text-4xl font-bold mb-3 text-primary'
+          data-aos='fade-right'
+        >
+          <span className='font-bold text-quinary'>DEPARTEMEN</span>
+          <br />
+          MAINTENANCE
+        </h2>
+      </SectionTitle>
       <FourMemberCard members={departemenMaintenance} />
     </section>
   );
