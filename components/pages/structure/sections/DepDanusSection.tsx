5675f2897f606699ced36d435265b8c3bc000094
Wrseno <warsenosetyono@gmail.com>
2024-07-25 15:50:14 +0700

    feat: change section title props

--- components/pages/structure/sections/DepDanusSection.tsx
+++ components/pages/structure/sections/DepDanusSection.tsx
@@ -5,15 +5,16 @@ import React from "react";
 export default function DepDanusSection() {
   return (
     <section className='h-auto py-16 px-4' id='danus'>
-      <SectionTitle
-        firstTitle='danus'
-        spanTitle='departemen'
-        textColor='text-quinary'
-        spanColor='text-primary'
-        useBr={false}
-        useLeftHr
-        hrWidth='w-full'
-      />
+      <SectionTitle useLeftHr hrWidth='w-full'>
+        <h2
+          className='text-2xl md:text-4xl font-bold mb-3 text-quinary'
+          data-aos='fade-left'
+        >
+          <span className='font-bold text-primary'>DEPARTEMEN</span>
+          <br />
+          DANUS
+        </h2>
+      </SectionTitle>
       <FiveMemberCard members={departemenDanus} />
     </section>
   );
