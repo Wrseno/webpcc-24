c6249148f10b8aea1e907c7b53718cea29d738d4
Wrseno <warsenosetyono@gmail.com>
2024-07-25 15:51:17 +0700

    feat: change section title props

--- components/pages/structure/sections/DivKrtSection.tsx
+++ components/pages/structure/sections/DivKrtSection.tsx
@@ -4,15 +4,16 @@ import {divisiKrt} from "@/data/data";
 export default function DivKrtSection() {
   return (
     <section className='h-auto py-16 px-4' id='krt'>
-      <SectionTitle
-        firstTitle='krt'
-        spanTitle='divisi'
-        textColor='text-primary'
-        spanColor='text-quinary'
-        useBr={false}
-        useRightHr
-        hrWidth='w-full'
-      />
+      <SectionTitle useRightHr hrWidth='w-full'>
+        <h2
+          className='text-2xl md:text-4xl font-bold mb-3 text-primary'
+          data-aos='fade-right'
+        >
+          <span className='font-bold text-quinary'>DIVISI</span>
+          <br />
+          KRT
+        </h2>
+      </SectionTitle>
       <FiveMemberCard members={divisiKrt} />
     </section>
   );
