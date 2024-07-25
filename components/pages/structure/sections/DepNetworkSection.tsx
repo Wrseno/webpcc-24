9cf05e8879cde7c56b82b254e47d1d47fca57a99
Wrseno <warsenosetyono@gmail.com>
2024-07-25 15:50:43 +0700

    feat: change section title props

--- components/pages/structure/sections/DepNetworkSection.tsx
+++ components/pages/structure/sections/DepNetworkSection.tsx
@@ -5,15 +5,16 @@ import React from "react";
 export default function DepNetworkSection() {
   return (
     <section className='h-auto py-16 px-4' id='network'>
-      <SectionTitle
-        firstTitle='network'
-        spanTitle='departemen'
-        textColor='text-quinary'
-        spanColor='text-primary'
-        useBr={false}
-        useRightHr
-        hrWidth='w-full'
-      />
+      <SectionTitle useRightHr hrWidth='w-full'>
+        <h2
+          className='text-2xl md:text-4xl font-bold mb-3 text-quinary'
+          data-aos='fade-right'
+        >
+          <span className='font-bold text-primary'>DEPARTEMEN</span>
+          <br />
+          NETWORK
+        </h2>
+      </SectionTitle>
       <FiveMemberCard members={departemenNetwork} />
     </section>
   );
