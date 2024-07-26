acb5fed92d6608189fdec003a31890bbb7f274c9
Wrseno <warsenosetyono@gmail.com>
2024-07-26 11:30:16 +0700

    feat: change section title props

--- components/pages/structure/sections/SekretarisSection.tsx
+++ components/pages/structure/sections/SekretarisSection.tsx
@@ -4,15 +4,14 @@ import {sekretaris} from "@/data/data";
 export default function SekretarisSection() {
   return (
     <section className='h-auto py-16 px-4' id='sekretaris'>
-      <SectionTitle
-        firstTitle='Sekretaris'
-        spanTitle=''
-        textColor='text-quinary'
-        useBr={false}
-        useLeftHr
-        hrWidth='w-full'
-      />
-
+      <SectionTitle useLeftHr hrWidth='w-full'>
+        <h2
+          className='text-2xl md:text-4xl font-bold mb-3 text-quinary'
+          data-aos='fade-left'
+        >
+          SEKRETARIS
+        </h2>
+      </SectionTitle>
       <ThreeMemberCard members={sekretaris} />
     </section>
   );
