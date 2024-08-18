b1be50dc645f86677f8200d580f037f35351597b
mashaydar <haydar130305@gmail.com>
2024-08-18 20:24:35 +0700

    feat: update some features

--- components/pages/structure/sections/StructureHero.tsx
+++ components/pages/structure/sections/StructureHero.tsx
@@ -1,22 +1,22 @@
-import {SectionTitle} from "@/components";
-import OrgChart from "@/components/common/OrgChart";
+import { SectionTitle } from "@/components";
+
+// import OrgChart from "@/components/common/OrgChart";
+import Image from "next/image";
 
 export default function StructureHero() {
   return (
     <section>
-      <div className='text-center mb-16 sm:mb-32'>
-        <SectionTitle useLeftHr useRightHr hrWidth='w-full'>
-          <h2
-            className='text-2xl md:text-4xl font-bold mb-3 text-primary'
-            data-aos='fade-up'
-          >
-            <span className='font-bold text-quinary'>STRUKTUR</span>
+      <div className="text-center mb-8">
+        <SectionTitle useLeftHr useRightHr hrWidth="w-full">
+          <h2 className="text-2xl md:text-4xl font-bold mb-3 text-primary" data-aos="fade-up">
+            <span className="font-bold text-quinary">STRUKTUR</span>
             <br />
             ORGANISASI
           </h2>
         </SectionTitle>
       </div>
-      <OrgChart />
+      {/* <OrgChart /> */}
+      <Image src="/images/struktur.png" className="w-full h-auto mx-auto my-0" width={1000} height={1000} alt="apa coba" />
     </section>
   );
 }
